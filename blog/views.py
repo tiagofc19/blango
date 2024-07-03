b38c18f68e8470ac630bca21aa995b228828e0cc
Tiago Coelho <tiagofc2000@gmail.com>
2024-07-03 08:15:50 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,16 @@ from blog.forms import CommentForm
 from .models import Post
 from django.utils import timezone
 import logging
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
 
 logger = logging.getLogger(__name__)
 
 def index(request):
-  posts = Post.objects.filter(published_at__lte = timezone.now())
-  logger.debug("Got %d posts", len(posts))
-  return render(request, 'blog/index.html', {"posts": posts})
-
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
+    return render(request, "blog/index.html", {"posts": posts})
+    
 def post_detail(request, slug):
   post = get_object_or_404(Post, slug = slug)
   if request.user.is_active:
